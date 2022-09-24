984af64005683111ddf78a82a5955d1e3a3af86d
thurcst <arthurhenriqueanibal@gmail.com>
2022-09-24 18:18:15 -0300

    wip: corrigindo classes de request

--- apisemglu/semglu/serializers.py
+++ apisemglu/semglu/serializers.py
@@ -1,49 +1,62 @@
+from itertools import product
 from rest_framework import serializers
 from .models import User, Safety, Report, Brand, Product
 
-class UserSerializer(serializers.ModelSerializer):
 
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = User
-        fields = '__all__'
+        fields = "__all__"
+
 
 class SafetySerializer(serializers.ModelSerializer):
-
     class Meta:
 
         model = Safety
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ReportSerializer(serializers.ModelSerializer):
+
+    filePath = serializers.SerializerMethodField()
 
     class Meta:
 
         model = Report
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_filePath(self, obj):
+        request = self.context.get("request")
+        filePath_temp = obj.filePath.url
+        return request.build_absolute_uri(filePath_temp)
+
 
 class BrandSerializer(serializers.ModelSerializer):
+
+    logoPath = serializers.SerializerMethodField()
 
     class Meta:
 
         model = Brand
-        fields = '__all__'
+        fields = "__all__"
 
-    def get_photo_url(self, obj):
-        request = self.context.get('request')
-        photo_url = obj.fingerprint.url
-        return request.build_absolute_uri(photo_url)
+    def get_logoPath(self, obj):
+        request = self.context.get("request")
+        logoPath_temp = obj.logoPath.url
+        return request.build_absolute_uri(logoPath_temp)
+
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    picturePath = serializers.SerializerMethodField()
 
     class Meta:
 
         model = Product
-        fields = '__all__'
+        fields = "__all__"
 
-    def get_photo_url(self, obj):
-        request = self.context.get('request')
-        photo_url = obj.fingerprint.url
-        return request.build_absolute_uri(photo_url)
-    
-
+    def get_picturePath(self, obj):
+        request = self.context.get("request")
+        picturePath_temp = obj.picturePath.url
+        return request.build_absolute_uri(picturePath_temp)
--- apisemglu/semglu/views.py
+++ apisemglu/semglu/views.py
@@ -1,5 +1,4 @@
-from http.client import HTTPResponse
-from rest_framework import generics
+from multiprocessing import context
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import User, Safety, Report, Brand, Product
@@ -13,52 +12,129 @@ from .serializers import (
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
+
+from rest_framework import generics
+
+from pprint import pprint
 
 # Cria a view da lista completa de usuários
 class UserList(APIView):
     def get(self, request):
         user_list = User.objects.all()  ## Busca os objetos
-
-        filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
-        ordering_fields = ["idUser"]
-        filterset_fields = ["idUser", "nick", "createdAt"]
-
-        serializer = UserSerializer(user_list)
-
+        pprint(list(user_list))
+        serializer = UserSerializer(user_list, many=True)
+        pprint(serializer.data)
         return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, request):
-        serializer_class = UserSerializer
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request):
-        ...
+        return Response(status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
         return Response(status.HTTP_400_BAD_REQUEST)
 
 
 # Cria a view do detalhamento (GET, PUT, DELETE) de um único usuário a partir de sua idUser
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = UserSerializer
-    lookup_field = "idUser"  ## Marca como chave primária para a url a mesma da entidade
-    queryset = User.objects.all()
+class UserDetail(APIView):
+    def get_object(self, idUser):
+        try:
+            return User.objects.get(idUser)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, idUser):
+        user = self.get_object(idUser)
+        serializer = UserSerializer(user)
+        return Response(data=serializer.data)
+
+    def post(self, request):
+        serializer_class = UserSerializer
+        serialized = serializer_class(request.data)
+
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status.HTTP_201_CREATED)
+        return Response(data=serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, idUser):
+        target = self.get_object(idUser)
+
+        serializer = UserSerializer(target, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, idUser):
+        target = self.get_object(idUser)
+        try:
+            target.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 # Cria a view da lista completa de Safety (perdão pela não tradução)
-class SafetyList(generics.ListCreateAPIView):
+class SafetyList(APIView):
+    def get(self, request):
+        safety_list = Safety.objects.all()
+        filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
+        ordering_fields = ["idSafety"]
+        filterset_fields = ["idSafety", "category", "description"]
 
-    queryset = Safety.objects.all()
-    serializer_class = SafetySerializer
-    filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    ordering_fields = ["idSafety"]
-    filterset_fields = ["idSafety", "category", "description"]
+        serializer = SafetySerializer(safety_list, many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
 
 
 # Cria a view do detalhamento (GET, PUT, DELETE) de uma única safety a partir de sua idSafety
-class SafetyDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = SafetySerializer
-    lookup_field = "idSafety"
-    queryset = Safety.objects.all()
+class SafetyDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Safety.objects.get(pk)
+        except Safety.DoesNotExist:
+            raise Http404
+
+    def get(self, pk):
+        safety = self.get_object(pk)
+        serializer = SafetySerializer(safety)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer_class = SafetySerializer
+        serialized = serializer_class(request.data)
+
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status.HTTP_201_CREATED)
+        return Response(serialized.errors, status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        safety = self.get_object(pk)
+        serializer = SafetySerializer(safety, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, pk):
+        safety = self.get_object(pk)
+        safety.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
 
 
 # Filtro por range de data dos laudos
@@ -74,38 +150,133 @@ class ReportDateFilter(filters.FilterSet):
 
 
 # Cria a view da lista completa de laudos
-class ReportList(generics.ListCreateAPIView):
+class ReportList(APIView):
+    def get(self, request):
+        report_list = Report.objects.all()  ## Busca os objetos
+        serializer = ReportSerializer(
+            report_list, many=True, context={"request": request}
+        )
 
-    queryset = Report.objects.all()
-    serializer_class = ReportSerializer
-    filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    ordering_fields = ["idReport"]
-    filterset_fields = ["idReport"]
-    filter_class = ReportDateFilter
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def put(self):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def delete(self):
+        return Response(status.HTTP_400_BAD_REQUEST)
 
 
 # Cria a view do detalhamento (GET, PUT, DELETE) de um único laudo a partir de sua idReport
-class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = ReportSerializer
-    lookup_field = "idReport"
-    queryset = Report.objects.all()
+class ReportDetail(APIView):
+    def get_object(self, idReport):
+        try:
+            return Report.objects.get(idReport)
+        except Report.DoesNotExist:
+            raise Http404
+
+    def get(self, request, idReport):
+        report = self.get_object(idReport)
+        serializer = ReportSerializer(report, context={"request": request})
+        return Response(data=serializer.data)
+
+    def post(self, request):
+        serializer_class = ReportSerializer
+        serialized = serializer_class(request.data)
+
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status.HTTP_201_CREATED)
+        return Response(data=serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, idReport):
+        target = self.get_object(idReport)
+
+        serializer = ReportSerializer(target, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, idReport):
+        target = self.get_object(idReport)
+        try:
+            target.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 # Cria a view da lista completa de marcas
-class BrandList(generics.ListCreateAPIView):
+class BrandList(APIView):
+    def get(self, request):
+        brand_list = Brand.objects.all()  ## Busca os objetos
 
-    queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
-    filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    ordering_fields = ["idBrand"]
-    filterset_fields = ["idBrand", "brandName", "contact"]
+        # TODO: Corrigir sistema de filtros para o GET
+
+        filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
+
+        ordering_fields = ["idBrand"]
+        filterset_fields = ["idBrand", "brandName", "contact"]
+
+        serializer = BrandSerializer(
+            brand_list, many=True, context={"request": request}
+        )
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
 
 
 # Cria a view do detalhamento (GET, PUT, DELETE) de uma única marca a partir de sua idBrand
-class BrandDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = BrandSerializer
-    lookup_field = "idBrand"
-    queryset = Brand.objects.all()
+class BrandDetail(APIView):
+    def get_object(self, idBrand):
+        try:
+            return Brand.objects.get(pk=idBrand)
+        except Brand.DoesNotExist:
+            raise Http404
+
+    def get(self, request, idBrand):
+        brand = self.get_object(idBrand)
+        serializer = BrandSerializer(brand, context={"request": request})
+        return Response(data=serializer.data)
+
+    def post(self, request):
+        serializer_class = BrandSerializer
+        serialized = serializer_class(request.data)
+
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status.HTTP_201_CREATED)
+        return Response(data=serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, idBrand):
+        target = self.get_object(idBrand)
+
+        serializer = BrandSerializer(target, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, idBrand):
+        target = self.get_object(idBrand)
+        try:
+            target.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 # Filtro dos produtos
@@ -121,26 +292,64 @@ class ProductDateFilter(filters.FilterSet):
 
 
 # Cria a view da lista completa de produtos
-class ProductList(generics.ListCreateAPIView):
+class ProductList(APIView):
+    def get(self, request):
+        product_list = Product.objects.all()
+        serializer = ProductSerializer(
+            product_list, many=True, context={"request": request}
+        )
 
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    filters_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    ordering_fields = ["barCode"]
-    filterset_fields = [
-        "barCode",
-        "idBrand",
-        "idSafety",
-        "idReport",
-        "productName",
-        "productCategory",
-        "productIngredients",
-    ]
-    filter_class = ProductDateFilter
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        return Response(status.HTTP_400_BAD_REQUEST)
 
 
 # Cria a view do detalhamento (GET, PUT, DELETE) de um único produto a partir de sua barCode
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = ProductSerializer
-    lookup_field = "barCode"
-    queryset = Product.objects.all()
+class ProductDetail(APIView):
+    def get_object(self, barCode):
+        try:
+            product = Product.objects.get(pk=barCode)
+            pprint(product)
+            return product
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, barCode):
+        product = self.get_object(barCode)
+        print(product)
+        serializer = ProductSerializer(product, context={"request": request})
+        return Response(data=serializer.data)
+
+    def post(self, request):
+        serializer_class = ProductSerializer
+        serialized = serializer_class(request.data)
+
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status.HTTP_201_CREATED)
+        return Response(data=serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, barCode):
+        target = self.get_object(barCode)
+
+        serializer = ProductSerializer(target, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, barCode):
+        target = self.get_object(barCode)
+        try:
+            target.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
