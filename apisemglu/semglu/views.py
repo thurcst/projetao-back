348c119528d1a7dfa98b46e1d0efefe6764d4ffc
bruhenri12 <61971775+bruhenri12@users.noreply.github.com>
2022-09-24 20:21:47 -0300

    simplificando os imports

--- apisemglu/semglu/views.py
+++ apisemglu/semglu/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import User, Safety, Report, Brand, Product
-from .serializers import UserSerializer, SafetySerializer, ReportSerializer, BrandSerializer, ProductSerializer
+from .models import *
+from .serializers import *
 from .filters import *
 
 
